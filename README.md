10df7b8155a586cab186e7a97914b6d7cce36240
Lucien PESLIER <lucienpeslier@pm.me>
2025-11-07 09:39:24 +0800

    Initial commit: add game source files

---
+++ README.md
@@ -0,0 +1,4 @@
+# 🎮 Le Jeu De La Complicité - Version Complète
+
+## ✨ Nouvelles fonctionnalités
+... (content omitted for brevity)
---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'complicite-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+// ... (content omitted for brevity)
